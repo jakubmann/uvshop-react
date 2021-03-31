38d37ddf4a1785fd1022f0d1e37760bbd7d64ef8
Jakub Heřman Mann <jakub.h.mann@protonmail.com>
2021-03-31 23:56:43 +0200

    homepage zaklad

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export default class Home extends React.Component {
+    state = {
+        filteredProducts: []
+    }
+
+    filterProducts = (group) => {
+        this.setState({
+            products: this.props.products.filter(p => { /* podminka pro filtrovani vsech produktu */})
+        })
+    }
+    
     render() {
         return (
             <div className="homepage">
-                homepage
+                <div className="product-select">
+                    <div className="product-select__groups">
+                        <div className="product-select__group" onClick={this.filterProducts('kosile')}>
+                            kosile
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="trunk">
+                    {this.props.products.map(p => {
+                        return <div className="product">{p.title}</div>
+                    })}
+                </div>
             </div>
         )
     }
